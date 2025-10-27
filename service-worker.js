51bc55a3238d00bab8b44d3f41886b353398919b
Dionatan wiggers <dionegalato1212@gmail.com>
2025-10-26 21:04:21 -0300

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,18 @@
-const CACHE_NAME = "pixcom-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('achados-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/imagem.jpg',
+        '/icon-512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) return caches.delete(name);
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
